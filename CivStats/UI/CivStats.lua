967b9e7cf1ff465a5d51c56b53b25660f284f6b7
bayvakoof <bayvakoof@live.com>
2015-10-23 15:55:53 -0400

    Added tracking policies

--- CivStats/UI/CivStats.lua
+++ CivStats/UI/CivStats.lua
@@ -5,6 +5,7 @@
 include( "Demographics" );
 
 local bStatsSaveReligion = false;
+local bStatsSavePolicies = false;
 
 function SaveCivStats()
 	SaveDemographics();
@@ -13,14 +14,23 @@ function SaveCivStats()
 		SaveReligion();
 		bStatsSaveReligion = false;
 	end
+
+	if (bStatsSavePolicies) then
+		SavePolicies();
+		bStatsSavePolicies = false;
+	end
 end
 Events.ActivePlayerTurnStart.Add( SaveCivStats );
 
 function HandlePopupProcessed(popupInfoType)
+	-- policies
+	if (popupInfoType == ButtonPopupTypes.BUTTONPOPUP_CHOOSEPOLICY) then
+		bStatsSavePolicies = true;
+	end
+
 	-- religion
 	if (popupInfoType == ButtonPopupTypes.BUTTONPOPUP_FOUND_PANTHEON or popupInfoType == ButtonPopupTypes.BUTTONPOPUP_FOUND_RELIGION) then
 		bStatsSaveReligion = true;
-		print("Chose a pantheon/founded a religion/chose reformation!");
 	end
 end
 Events.SerialEventGameMessagePopupProcessed.Add(HandlePopupProcessed);
@@ -54,14 +64,13 @@ function SaveReligion()
 				local beliefType = string.lower(GetBeliefType(belief));
 				modUserData.SetValue(i, Locale.Lookup(belief.ShortDescription));
 				modUserData.SetValue(i .. "-type", beliefType);
-				--print("Belief " .. beliefType .. " is " .. Locale.Lookup(belief.ShortDescription));
 			end
 		end
 	elseif (player:HasCreatedPantheon()) then
 		local pantheonId = player:GetBeliefInPantheon();						
 		local belief = GameInfo.Beliefs[pantheonId];
-		--print("Pantheon (id" .. pantheonId .. ") is " .. Locale.Lookup(belief.ShortDescription));
 		modUserData.SetValue("1", Locale.Lookup(belief.ShortDescription));
+		modUserData.SetValue("1-type", "pantheon");
 	end
 end
 
@@ -76,5 +85,40 @@ function GetBeliefType(belief)
 		return Locale.Lookup("TXT_KEY_RO_BELIEF_TYPE_ENHANCER");
 	elseif(belief.Reformation) then
 		return Locale.Lookup("TXT_KEY_RO_BELIEF_TYPE_REFORMATION");
+	end
+end
+
+function SavePolicies()
+	local player = Players[Game.GetActivePlayer()];
+
+	local policiesTable = {}
+	
+	i = 0;
+	local policyInfo = GameInfo.Policies[i];
+	while policyInfo ~= nil do
+		if player:HasPolicy( i ) then
+			local branchName;			
+			local iBranch = policyInfo.PolicyBranchType
+			if (iBranch ~= nil) then
+				branchName = Locale.Lookup(GameInfo.PolicyBranchTypes[iBranch].Description);
+			else
+				-- policy is an opener
+				branchName = Locale.Lookup(policyInfo.Description);
+			end
+			
+			if (policiesTable[branchName] ~= nil) then 
+				policiesTable[branchName] = policiesTable[branchName] + 1;
+			else
+				policiesTable[branchName] = 0;
+			end
+		end
+
+		i = i + 1;
+		policyInfo = GameInfo.Policies[i];
+	end
+
+	modUserData = Modding.OpenUserData("civstats-policies", 1); 
+	for k,v in pairs(policiesTable) do
+		modUserData.SetValue(k, v);
 	end
 end
