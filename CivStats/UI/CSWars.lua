b6f9824c2b4f3f7ebcf6a7eb87fc3c865e766a59
bayvakoof <bayvakoof@live.com>
2015-11-29 10:14:34 -0500

    Implemented tracking wars

--- CivStats/UI/CSWars.lua
+++ CivStats/UI/CSWars.lua
@@ -8,45 +8,42 @@ function SetupWarSaving()
 	Modding.DeleteUserData("civstats-wars", 1)
 	warUserData = Modding.OpenUserData("civstats-wars", 1)  
 	
-	-- when active player declares war on someone, there's no notification 
-	Events.NotificationAdded.Add( HandleWarNotification )
-	Events.GameplayAlertMessage.Add( HandleUIMessage )
+	Events.WarStateChanged.Add( WarStateHandler )
 end
 
-function SaveWarStart(civilization, aggressor)
+function SaveWarStart(otherPlayer, aggressor)
+	local otherCiv = GameInfo.Civilizations[otherPlayer:GetCivilizationType()].ShortDescription
+
 	warUserData.SetValue("turn", Game.GetGameTurn())
-	warUserData.SetValue("civilization", "")
-	warUserData.SetValue("aggressor", "") -- whether active player initiated the war
+	warUserData.SetValue("civilization", otherCiv)
+	warUserData.SetValue("aggressor", aggressor) -- whether active player initiated the war
 end
 
-function SaveWarEnd()
+function SaveWarEnd(otherPlayer)
+	local otherCiv = GameInfo.Civilizations[otherPlayer:GetCivilizationType()].ShortDescription
+
 	warUserData.SetValue("turn", Game.GetGameTurn())
-	warUserData.SetValue("civilization", "")
+	warUserData.SetValue("civilization", otherCiv)
 end
 
--- this catches other players declaring war on you and peace deals you make
-function HandleWarNotification(notificationId, notificationType, toolTip, summary, gameValue, extraGameData)
-	if notificationType ~= NotificationTypes.NOTIFICATION_WAR_ACTIVE_PLAYER and
-	notificationType ~= NotificationTypes.NOTIFICATION_PEACE_ACTIVE_PLAYER then
+function WarStateHandler( iTeam1, iTeam2, bWar )
+	local otherTeam = nil
+	if iTeam1 == Game.GetActiveTeam() then
+		otherTeam = Teams[iTeam2]
+	elseif iTeam2 == Game.GetActiveTeam() then
+		otherTeam = Teams[iTeam1]
+	end
+
+	-- return if war doesn't involve active player
+	if otherTeam == nil then
 		return
 	end
 
-	local bIsPeace = notificationType == NotificationTypes.NOTIFICATION_PEACE_ACTIVE_PLAYER
-
-	-- e.g. [COLOR_NEGATIVE_TEXT]Player 1 has declared war on you![ENDCOLOR]
-	-- print(Players[gameValue]:GetName())
-
-	if bIsPeace then
-		SaveWarEnd()
+	local otherPlayer = Players[otherTeam:GetLeaderID()]
+	if bWar then
+		local isAggressor = (iTeam1 == Game.GetActiveTeam())
+		SaveWarStart(otherPlayer, isAggressor)
 	else
-		SaveWarStart(false)
+		SaveWarEnd(otherPlayer)
 	end
-end
-
--- this catches you declaring war on another player
-function HandleUIMessage(text)
-	print("Message! " .. text)
-	-- e.g. Message! You have declared war on Player 2!
-
-	-- TODO parse text to determine who DoWed on whom
 end
